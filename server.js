63a01484e08d39958a9d2d1ce1426367777a5a59
RHJL <rodrigogf@live.com>
2015-08-11 23:03:02 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//variable que contiene al servidor
+var app = require('http').createServer(index), io = require('socket.io').listen(app) , fs = require('fs');
+app.listen(3000, function() {
+
+});
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '123456',
+   database: 'prueba',
+   port: 3306
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      //console.log('Conexion correcta.');
+   }
+});
+//función que llamara a la página principal que el cliente podrá visualizar
+function index(req, res){
+fs.readFile(__dirname + '/pagina.html', function(err, data){
+res.end(data);
+});
+};
+io.on('connection', function(socket){
+
+ socket.on('message', function (mensaje) {
+var query = connection.query('INSERT INTO p1(nombre) VALUES(?)', [mensaje], function(error, result){
+   if(error){
+      throw error;
+   }else{
+      //console.log(result);
+   }
+ }
+);
+    }); 
+});
