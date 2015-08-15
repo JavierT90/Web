5e1d8de40c586620f6e574cc73b5a55d04fa2649
RHJL <rodrigogf@live.com>
2015-08-14 19:27:53 -0600

    Quinto commit

--- server.js
+++ server.js
@@ -27,9 +27,45 @@ res.end(data);
 };
 io.on('connection', function(socket){
 
- socket.on('message', function (mensaje) {
-if(isNaN(mensaje)){
-var query = connection.query('INSERT INTO p1(nombre) VALUES(?)', [mensaje], function(error, result){
+socket.on('solicitar_llenar', function () {
+var query = connection.query('SELECT evento_id,nombre FROM p1 where estado=1', function(error, result){
+      if(error){
+         throw error;
+      }else{
+         var resultado = result;
+         if(resultado.length > 0){
+	var contador=0;
+	var cadena_resultado=resultado[0].nombre+","+resultado[0].evento_id;
+	contador++;
+	while(resultado.length>contador){
+	var aux=resultado[contador].nombre+","+resultado[contador].evento_id;
+	cadena_resultado=cadena_resultado+";"+aux;
+	contador++;	
+	}
+	socket.emit('Llenar',cadena_resultado);
+         }else{
+            console.log('Registro no encontrado');
+		socket.emit('Llenar','');
+         }
+      }
+   }
+);
+});
+
+ socket.on('insertar_evento', function (mensaje) {
+if(isNaN(mensaje.nombre)){
+var error_fecha=0;
+var f01=mensaje.fi.split("-");
+var mes1=parseInt(f01[1]);
+var dia1=parseInt(f01[2]);
+if(mes1==2&&dia1>29){
+error_fecha=1;
+}
+if((mes1==4||mes1==6||mes1==9||mes1==11)&&dia1>30){
+error_fecha=1;
+}
+if(error_fecha==0){
+var query = connection.query('INSERT INTO p1(nombre,estado,inicio,fin) VALUES(?,?,?,?)', [mensaje.nombre,1,mensaje.fi,mensaje.ff], function(error, result){
    if(error){
 	socket.emit('resultado','error');
       throw error;
@@ -38,9 +74,33 @@ socket.emit('resultado','correcto');
       //console.log(result);
    }
  }
-);}
+);
+}
+else{
+socket.emit('resultado','error01_fecha');
+}}
 else{
 socket.emit('resultado','error1');
    }
     }); 
+
+ socket.on('terminar_evento', function (id_evento) {
+//console.log(id_evento);
+var query = connection.query('UPDATE p1 SET estado=0 WHERE evento_id=? AND estado=1', [id_evento], function(error, result){
+   if(error){
+	socket.emit('resultado2','error');
+      throw error;
+   }else{
+socket.emit('resultado2','correcto');
+      //console.log(result);
+   }
+ }
+);
+    }); 
+
 });
+
+
+
+
+
--- test/test-server.js
+++ test/test-server.js
@@ -15,7 +15,10 @@ it('Deberia dar un resultado correcto', function(done){
   var client1 = io.connect(socketURL, options);
 
   client1.on('connect', function(data){
-    client1.emit('message', 'evento_prueba');
+    client1.emit('insertar_evento', {
+						nombre:'evento_prueba',
+						fi:'2019-11-24 17:15:10',
+						ff:'2019-11-24 19:15:10'});
 
   });
 
@@ -31,7 +34,10 @@ it('Deberia dar un resultado incorrecto', function(done){
   var client1 = io.connect(socketURL, options);
 
   client1.on('connect', function(data){
-    client1.emit('message', '14');
+    client1.emit('insertar_evento', {
+						nombre:'14',
+						fi:'2019-11-24 17:15:10',
+						ff:'2019-11-24 19:15:10'});
 
   });
 
@@ -42,6 +48,81 @@ it('Deberia dar un resultado incorrecto', function(done){
     
   });
 });
+
+it('Deberia dar un resultado de error por la fecha', function(done){
+  var client1 = io.connect(socketURL, options);
+
+  client1.on('connect', function(data){
+    client1.emit('insertar_evento', {
+						nombre:'evento_prueba',
+						fi:'2019-04-31 17:15:10',
+						ff:'2019-04-31 19:15:10'});
+
+  });
+
+  client1.on('resultado', function(respuesta){
+      respuesta.should.equal("error01_fecha");
+      client1.disconnect();
+      done();
+    
+  });
+});
+
+it('Deberia dar un resultado de error por la fecha de febrero', function(done){
+  var client1 = io.connect(socketURL, options);
+
+  client1.on('connect', function(data){
+    client1.emit('insertar_evento', {
+						nombre:'evento_prueba',
+						fi:'2019-02-30 17:15:10',
+						ff:'2019-02-30 19:15:10'});
+
+  });
+
+  client1.on('resultado', function(respuesta){
+      respuesta.should.equal("error01_fecha");
+      client1.disconnect();
+      done();
+    
+  });
+});
+
+it('Deberia terminar el evento', function(done){
+  var client1 = io.connect(socketURL, options);
+
+  client1.on('connect', function(data){
+    client1.emit('insertar_evento', {
+						nombre:'evento_terminado',
+						fi:'2019-11-24 17:15:10',
+						ff:'2019-11-24 19:15:10'});
+  });
+
+ client1.on('resultado', function(respuesta){
+      client1.emit('solicitar_llenar');
+    
+  });
+
+client1.on("Llenar", function (data) {
+	data2=data.split(";");
+	if(data2.length>0){
+	var contador=data2.length-1;
+	while(0<contador){
+	d1=data2[contador].split(",");
+	if('evento_terminado'== d1[0]){
+	client1.emit('terminar_evento',d1[1]);
+	contador=0;}
+	contador--;}
+	}
+        });
+
+  client1.on('resultado2', function(respuesta){
+      respuesta.should.equal("correcto");
+      client1.disconnect();
+      done();
+    
+  });
+});
+
 
 });
 
